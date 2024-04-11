9482aa7f0a087281a463998638856f860ff3f098
Tafarel Yan <tafarel.yan@gmail.com>
2024-04-11 13:09:12 -0300

    📝 Add typing ListNode

--- leet_code/add_two_numbers.py
+++ leet_code/add_two_numbers.py
@@ -5,7 +5,7 @@ class ListNode:
 
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         dummy = ListNode()
         currentNode = dummy
         carry = 0
