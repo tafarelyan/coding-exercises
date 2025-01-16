38ac9c26d2e8cb217e644ac6c4c47f04fdc9f02c
Tafarel Yan <tafarel.yan@gmail.com>
2025-01-16 10:49:19 -0300

    🎉 Start from the beginning

--- hacker_rank/left_rotation.py
+++
@@ -1,9 +0,0 @@
-def rotLeft(a, d):
-    for _ in range(d):
-        element = a.pop(0)
-        a.append(element)
-
-    return a
-
-if __name__ == '__main__':
-    print(rotLeft(5, 4))
--- tech_interview_handbook/template.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def function(self, arg):
-        return arg
-
-if __name__ == '__main__':
-    solution = Solution()
-    print(solution.function('Hello World'))
